e4c02845cbd9be9c0b9d89162fa6cc9e62ba79b4
Pedro Marques <pedrommarquesm@gmail.com>
2024-01-14 15:45:09 +0000

    init nextjs app with tailwind

---
+++ app/src/components/base-block.tsx
@@ -0,0 +1,12 @@
+import { classed } from "@/config/classed";
+
+const BaseBlock = classed(
+  "button",
+  "flex",
+  "h-6 w-6",
+  "border-2 border-white",
+  "border-b-2 border-r-2",
+  "border-b-[gray] border-r-[gray]"
+);
+
+export default BaseBlock;
